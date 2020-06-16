f767246c7cd9449ee69fb0126a0a6f077c7b0c9a
tq-o <onquocthai1106@gmail.com>
2020-06-16 13:58:20 -0400

    uxrich

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css';
+// import owl from './owl.gif'
+class App extends Component {
+  render(){
+    return (
+      
+      <body>
+        {/* <div className = "container" id ="container">
+          fsdfgsdgfdsfs
+        </div> */}
+        <button className = "button sign-out">sign out</button>
+        <button className = "button home">home</button>
+        <button className = "button list">list</button>
+
+
+      </body>
+      
+    );
+  }
+}
+
+export default App;
