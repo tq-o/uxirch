d4079cb0887a0e7cd0425e824da4bda4ec7af550
tq-o <onquocthai1106@gmail.com>
2020-06-18 17:40:19 -0400

    version-3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import './App.css';
-// import owl from './owl.gif'
+import React, { Component } from "react";
+import "./App.css";
+import instbtn from "./img/inst-btn.png";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cat1: 'coconut',
-      cat2: 'coconut',
-      cat3: 'coconut'
+      cat1: "coconut",
+      cat2: "coconut",
+      cat3: "coconut",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.mouseOver = this.mouseOver.bind(this);
   }
 
   handleChange(event) {
@@ -20,60 +22,83 @@ class App extends Component {
   }
 
   handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.cat1 + ',' + this.state.cat2 + ',' + this.state.cat3);
+    alert(
+      "Your favorite flavor is: " +
+        this.state.cat1 +
+        "," +
+        this.state.cat2 +
+        "," +
+        this.state.cat3
+    );
     event.preventDefault();
   }
 
-  render(){
+  mouseOver(event) {
+    alert("Hi");
+  }
+
+  render() {
     return (
-      
       <body>
-        {/* <div className = "container" id ="container">
-          fsdfgsdgfdsfs
-        </div> */}
-        <button className = "button sign-out"><span>sign out</span></button>
-        <button className = "button home">home</button>
-        <button className = "button list">list</button>
+        {/* <div className="rectangle" /> */}
+        <button className="button sign-out">
+          <span>sign out</span>
+        </button>
+        <button className="button home">home</button>
+        <button className="button list">list</button>
+          <div className="rectangle">
+            <img
+              className="btn"
+              onMouseOver={this.mouseOver}
+              src={instbtn}
+              alt="loading"
+            ></img>
+            <form onSubmit={this.handleSubmit}>
+              <label className="cat1">
+                Category 1:
+                <select
+                  name="cat1"
+                  cat1={this.state.cat1}
+                  onChange={this.handleChange}
+                >
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </label>
 
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <label className = "cat1">
-              Category 1:
-              <select name ="cat1" cat1={this.state.cat1} onChange={this.handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-              </select>
-            </label>
-            
-
-            <label className = "cat2">
-              Category 2:
-              <select name ="cat2" cat2={this.state.cat2} onChange={this.handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-              </select>
-            </label>
-            <label className = "cat3">
-              Category 3:
-              <select name ="cat3" cat3={this.state.cat3} onChange={this.handleChange}>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-              </select>
-            </label>
-            <input className = "submit" type="submit" value="Submit" />
-          </form>
-        </div>
-
-
+              <label className="cat2">
+                Category 2:
+                <select
+                  name="cat2"
+                  cat2={this.state.cat2}
+                  onChange={this.handleChange}
+                >
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </label>
+              <label className="cat3">
+                Category 3:
+                <select
+                  name="cat3"
+                  cat3={this.state.cat3}
+                  onChange={this.handleChange}
+                >
+                  <option value="grapefruit">Grapefruit</option>
+                  <option value="lime">Lime</option>
+                  <option value="coconut">Coconut</option>
+                  <option value="mango">Mango</option>
+                </select>
+              </label>
+              <input className="submit" type="submit" value="Submit" />
+              <text className="disclaimer">Disclaimer: We love sloths</text>
+            </form>
+          </div>
       </body>
-      
-      
     );
   }
 }
