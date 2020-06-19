9360699d1aa4a0eeb4a572e4e7dfbd5b842897f3
briansun1210 <44716335+briansun1210@users.noreply.github.com>
2020-06-19 11:57:15 -0400

    brian

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import './App.css';
-// import owl from './owl.gif'
+
+import button from './img/forgot-password-button.png';
+import circle from './img/circle.png';
+
+
 class App extends Component {
+
   render(){
     return (
       
@@ -9,15 +14,43 @@ class App extends Component {
         {/* <div className = "container" id ="container">
           fsdfgsdgfdsfs
         </div> */}
-        <button className = "button sign-out">sign out</button>
-        <button className = "button home">bitch</button>
-        <button className = "button list">list</button>
+       {/* <button className = "button sign-out">sign out</button>
+        <button className = "button home">here</button>
+      <button className = "button list">out</button>*/}
+
+        
+
+        <div className = "container">
+          
+          <img className = "circleImage" src = {circle} alt="alternatetext"></img>
+
+          <a style = {{backgroundColor: 'transparent', border: 'none'}}href="#">
+          <img className = "image" src = {button} alt="alternatetext"></img>
+          </a>
+
+          <button className = "button sign-in">sign in</button>
+
+          
+          <input className = "enterUserName" placeholder = "username"></input>
+          <input className = "enterPassword" placeholder = "password"></input>
+          
 
 
+        </div>
+
+          
+
+        
+
+
+       
       </body>
+
+
       
     );
   }
 }
+
 
 export default App;
