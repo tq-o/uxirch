bd7f1561c34219d51c851bc5b49267067d0c79c5
briansun1210 <44716335+briansun1210@users.noreply.github.com>
2020-06-22 15:27:08 -0400

    where is my shit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 // import owl from './owl.gif'
 class App extends Component {
+<<<<<<< Updated upstream
   render(){
     return (
       
@@ -16,6 +17,68 @@ class App extends Component {
 
       </body>
       
+=======
+
+  constructor(props) {
+    super(props)
+    this.state = {userN: '', userP: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+   };
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+   }
+
+  handleSubmit(event) {
+    alert('your username: ' + this.state.userN + 'your password: ' + this.state.userP );
+    event.preventDefault();
+   }
+
+
+  render(){
+    return (
+
+      <body>
+
+        <div className = "container" >
+          
+          <img className = "circleImage" src = {circle} alt="alternatetext"></img>
+
+          <a style = {{backgroundColor: 'transparent', border: 'none'}}href="">
+          <img className = "image" src = {button} alt="alternatetext"></img>
+          </a>
+
+          <form onSubmit = {this.handleSubmit}> 
+
+          <input 
+          name = "userN"
+          userN = {this.state.userN}
+          onChange = {this.handleChange}
+          className = "enterUserName" 
+          placeholder = "username">
+          </input>
+
+          <input 
+          name = "userP"
+          userP = {this.state.userP}
+          onChange = {this.handleChange}
+          className = "enterPassword" 
+          placeholder = "password">
+          </input>
+
+          <button 
+          type = "submit"
+          className = "button sign-in">sign in
+          </button>
+
+          </form>
+
+        </div>
+ 
+      </body>
+>>>>>>> Stashed changes
     );
   }
 }
