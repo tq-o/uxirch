307bb4d3fb8c0ac6e5e89d67727bb313eb73c664
briansun1210 <44716335+briansun1210@users.noreply.github.com>
2020-07-08 15:22:12 -0400

    recommendation yay

--- src/game-rec/game-rec.py
+++ src/game-rec/game-rec.py
@@ -1,2 +1,25 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+metadata =pd.read_csv('steam_games.csv', low_memory=False)
+
+tfidf = TfidfVectorizer(stop_words='english')
+metadata['desc_snippet']= metadata['desc_snippet'].fillna('')
+
+tfidf_matrix = tfidf.fit_transform(metadata['desc_snippet'])
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+indices = pd.Series(metadata.index, index = metadata['name']).drop_duplicates()
+
+def get_recommendations(name, consine_sim = cosine_sim):
+    idx = indices[name]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse = True)
+    sim_scores = sim_scores[1:11]
+
+    game = [i[0] for i in sim_scores]
+    return metadata['name'].iloc[game]
+
+print(get_recommendations('DOOM'))
